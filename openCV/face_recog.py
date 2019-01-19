8b3be36c92a0057058f775cf25c6229371041953
jamesyaputra <jamesyaputra1704@gmail.com>
2019-01-19 13:36:56 +0800

    Added emotion recognition model

--- openCV/face_recog.py
+++ openCV/face_recog.py
@@ -1,17 +1,15 @@
 import cv2
 import numpy as np
+from keras.preprocessing import image
+from keras.models import model_from_json
 
-face_cascade = detect.cv2.cascadeClassifier("./cascades/data/haarcascade_frontalface_alt2.xml")
-cap = cv2.VideoCapture()
+face_cascade = cv2.CascadeClassifier("./cascades/data/haarcascade_frontalface_alt2.xml")
+cap = cv2.VideoCapture(0)
 
 while (True):
     ret, frame = cap.read()
-
-    # Grayscale
-    gray = cv2.cvtColor(frame, cv2.COLORBGR2GRAY)
-
-    # Detect faces
-    faces = cv2.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
     for (x, y, w, h) in faces:
         color = (0, 0, 255) # RED rectangle
         stroke = 2
@@ -19,9 +17,9 @@ while (True):
         y_end = y + h
         cv2.rectangle(frame, (x, y), (x_end, y_end), color, stroke)
 
+    cv2.imshow("frame", frame)
     if (cv2.waitKey(20) & 0xFF == ord("q")):
         break
         
-
 cap.release()
 cv2.destroyAllWindows()
