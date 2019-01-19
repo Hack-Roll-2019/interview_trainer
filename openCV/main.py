93bf1d0c079ef017af3ddad6d0f39105eddfe9aa
jamesyaputra <jamesyaputra1704@gmail.com>
2019-01-19 22:23:17 +0800

    Fixed directory in CV2 vid capture

--- openCV/main.py
+++ openCV/main.py
@@ -18,44 +18,44 @@ def run_recognition(video_dir):
     emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
     happy_count = 0
-    # if (cap.isOpened() == False): 
-    while (cap.isOpened()):
-        ret, frame = cap.read()
-        
-        if (np.shape(frame) == ()):
-            break
+    if (cap.isOpened() != False): 
+        while (cap.isOpened()):
+            ret, frame = cap.read()
+            
+            if (np.shape(frame) == ()):
+                break
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
 
-        for (x, y, w, h) in faces:
-            roi_gray = gray[y:y+h, x:x+w]
-            roi_color = frame[y:y+h, x:x+w]
-            eyes = eyes_cascade.detectMultiScale(roi_gray, 1.1, 3)
+            for (x, y, w, h) in faces:
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = frame[y:y+h, x:x+w]
+                eyes = eyes_cascade.detectMultiScale(roi_gray, 1.1, 3)
 
-            for (ex, ey, ew, eh) in eyes:
-                cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+                for (ex, ey, ew, eh) in eyes:
+                    cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
 
-            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
 
-            detected_face = frame[int(y):int(y + h), int(x):int(x + w)]
-            detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)
-            detected_face = cv2.resize(detected_face, (48, 48))
+                detected_face = frame[int(y):int(y + h), int(x):int(x + w)]
+                detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY)
+                detected_face = cv2.resize(detected_face, (48, 48))
 
-            img_pixels = image.img_to_array(detected_face)
-            img_pixels = np.expand_dims(img_pixels, axis = 0)
-            img_pixels /= 255
+                img_pixels = image.img_to_array(detected_face)
+                img_pixels = np.expand_dims(img_pixels, axis = 0)
+                img_pixels /= 255
 
-            predictions = model.predict(img_pixels)
-            max_index = np.argmax(predictions[0])
-            emotion = emotions[max_index]
+                predictions = model.predict(img_pixels)
+                max_index = np.argmax(predictions[0])
+                emotion = emotions[max_index]
 
-            if (emotion == "happy"):
-                happy_count += 1
+                if (emotion == "happy"):
+                    happy_count += 1
 
-        cv2.imshow("frame", frame)
-        if cv2.waitKey(20) & 0xFF == ord('q'):
-            break
+            cv2.imshow("frame", frame)
+            if cv2.waitKey(20) & 0xFF == ord('q'):
+                break
 
     cap.release()
     cv2.destroyAllWindows()
