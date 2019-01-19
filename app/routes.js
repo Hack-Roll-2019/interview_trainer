d9deaae34f517bd3aa695b0c67fbde879f223547
Matthew Alexander <alexandermatthew12@gmail.com>
2019-01-19 16:01:03 +0800

    Displayed website with backend

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,19 @@
-module.exports = (app, connection) => {
+module.exports = (app, connection, auth) => {
     app.get('/', (req, res) => {
-        res.render()
-    })
-}
+        res.render("landing.ejs");
+    });
+
+    app.get('/signin', (req, res) => {
+        res.render("signin.ejs");
+    });
+
+    app.get('/signup', (req, res) => {
+        res.render("signup.ejs");
+    });
+
+    app.post('/signup', (req, res) => {
+        req.body
+    });
+
+
+}   
