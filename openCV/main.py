52eaf67dfaef24ce5792c3677d6888e17bd75ebe
jamesyaputra <jamesyaputra1704@gmail.com>
2019-01-20 00:29:32 +0800

    Added additional dependencies

--- openCV/main.py
+++ openCV/main.py
@@ -1,9 +1,38 @@
 import sys
-import cv2
-import numpy as np
+import time
 from pathlib import Path
-from keras.preprocessing import image
+
+import cv2
+import dlib
+import imutils
+import numpy as np
+
+from imutils import face_utils
 from keras.models import model_from_json
+from keras.preprocessing import image
+from scipy.spatial import distance as dist
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+
+COUNTER = 0
+TOTAL = 0
+
+def eye_aspect_ratio(eye):
+	# compute the euclidean distances between the two sets of
+	# vertical eye landmarks (x, y)-coordinates
+	A = dist.euclidean(eye[1], eye[5])
+	B = dist.euclidean(eye[2], eye[4])
+ 
+	# compute the euclidean distance between the horizontal
+	# eye landmark (x, y)-coordinates
+	C = dist.euclidean(eye[0], eye[3])
+ 
+	# compute the eye aspect ratio
+	ear = (A + B) / (2.0 * C)
+ 
+	# return the eye aspect ratio
+	return ear
 
 def run_recognition(video_dir):
     # OpenCV setup
@@ -29,6 +58,13 @@ def run_recognition(video_dir):
 
     emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
 
+    # Eye blink detector setup
+    # detector = dlib.get_frontal_face_detector()
+    # predictor = dlib.shape_predictor()
+
+    # (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    # (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
     happy_count = 0
     if (cap.isOpened() != False): 
         while (cap.isOpened()):
@@ -37,6 +73,7 @@ def run_recognition(video_dir):
             if (np.shape(frame) == ()):
                 break
 
+            frame = imutils.resize(frame, width = 450)
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             faces = face_cascade.detectMultiScale(gray, scaleFactor = 1.1, minNeighbors = 5)
 
